efc47edb2a66b6202de3245a803f4d225d041dc6
Tyler S. Cook <145003601@mvhsrop901-20.vusdnoc.vusd.k12.ca.us>
2015-11-02 10:57:54 -0800

    added useless variable

--- src/monopoly/project/MonopolyProject.java
+++ src/monopoly/project/MonopolyProject.java
@@ -32,7 +32,7 @@ public class MonopolyProject extends JFrame implements Runnable {
     public static Image image;
     Graphics2D g;
     
-
+    boolean hello = false;
     static Dimension dim;
 
     public static boolean animateFirstTime = true;
